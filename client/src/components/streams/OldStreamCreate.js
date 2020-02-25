73238ed35bafff13e601122aaaddc622df239965
markhorvath <markhorvath7@gmail.com>
2020-02-24 20:55:51 -0500

    StreamForm component, refactor StreamCreate nd StreamEdit, more reduxForm work

---
+++ client/src/components/streams/OldStreamCreate.js
@@ -0,0 +1,86 @@
+// import React from 'react';
+// import { Field, reduxForm } from 'redux-form';
+// //import connect so the action-creator createStream can act on this component
+// import { connect } from 'react-redux';
+// import { createStream } from '../../actions';
+
+// class StreamCreate extends React.Component {
+//     renderError({ error, touched }) {
+//         if(error && touched) {
+//             return (
+//                 <div className="ui error message">
+//                     <div className="header">{error}
+//                     </div>
+//                 </div>
+//             );
+//         }
+//     }
+//     //changed to arrow function to get this.rendorError to work
+//     renderInput = ({ input, label, meta }) => {
+//         const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+//         console.log(meta);
+//         //originaly was "formProps" in arg,
+//         //also <input value={formProps.input.value} onChange={formProps.input.onChange} /> but
+//         //we implemented destructuring, so basically this should have value and onChange attributes
+//         //equal to whatever it is in the "formProps.input"
+//         return (
+//             <div className={className}>
+//                 <label>{label}</label>
+//                 <input {...input} />
+//                 {this.renderError(meta)}
+//             </div>
+//         )
+//     }
+
+//     onSubmit = (formValues) => {
+//         //this is calling the action creator createStream
+//         this.props.createStream(formValues);
+//     }
+
+//     render() {
+//         //component prop is added to Field to actually be able to render the Field (will thro error if its not there)
+//         return (
+//             <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+//                 <Field name="title" component={this.renderInput} label="Enter Title: " />
+//                 <Field name="description" component={this.renderInput} label="Enter Description: " />
+//                 <button className="ui button primary">Submit</button>
+//             </form>
+//         )
+//     }
+// }
+
+// //this validate function needs to get connected to the redux-form so it gets used
+// //we do this by passing it in the reduxForm function we export at the bottom (and because it's
+// //just validate: validate we can destructure it)
+// const validate = (formValues) => {
+//     const errors = {};
+
+//     if (!formValues.title) {
+//         //only runs if user did not enter title
+//         errors.title = "Title is required";
+//     }
+//     if (!formValues.description) {
+//         errors.description = "Description is required";
+//     }
+//     //the idea here is that our errors object has properties with an IDENTICAL name to the 'name' attributes
+//     //we provided the to Field components above (i.e. 'title' and 'description'), so if the error object has the same name
+//     //and contains a string (with the error message) that string will get passed down to component={this.renderInput} within that Field
+//     return errors;
+
+//     //you can try console.log(meta) under renderInput() to see that there's an error property that comes with 'meta' and which should have
+//     //the corresponding error message
+// }
+
+// //remove the export default
+// const formWrapped =  reduxForm({
+//     form: 'createStream',
+//     validate
+// })(StreamCreate);
+
+// export default connect(null, { createStream })(formWrapped);
+
+// //The code below is the same as the 6 lines above which is just a nicer looking syntax
+// // export default connect(reduxForm({
+// //     form: 'createStream',
+// //     validate
+// // })(StreamCreate));
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = (props) => {
+//we turned this into class-based to add componentDidMount and call fetchStream
+//because the component needed to be able to pull that info on it's own
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues);
+    }
+
     //props has a match.params.id value because that's the variable name in the App.js Route component path attr
-    console.log('streamEdit props: ', props);
-    return (
-        <div>StreamEdit</div>
-    )
+    render() {
+        console.log('streamEdit props: ', this.props);
+        if(!this.props.stream) {
+                return <div>Loading...</div>;
+        }
+        //initialValues is special prop with redux forms i think*
+        //we are using title and description property names because they're in the StreamForm Field Components
+        //initialValues={{ title: 'EDIT ME', description: 'Change me too'}}
+
+        //this is more descructuring, he used lodash _.pick methodd in the course like so:
+        //initialValues={_.pick(this.props.stream, 'title', 'description')}
+        const { title, description } = this.props.stream;
+        return (
+            <div>
+            <h3>Edit a Stream</h3>
+            <StreamForm
+                initialValues={{ title: title, description: description }}
+                onSubmit={this.onSubmit} />
+            </div>
+        )
+    }
 }
 
-export default StreamEdit;
+//ownProps is the 2nd argument that gets passed into mapStateToProps and is a reference to the props object of the component
+const mapStateToProps = (state, ownProps) => {
+    // console.log(ownProps);
+    // console.log(state.streams);
+    return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
