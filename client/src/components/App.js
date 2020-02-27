e5ad12b312da6136ec52f6374b8d1063a079b980
markhorvath <markhorvath7@gmail.com>
2020-02-26 20:28:30 -0500

    get streaming working with rmtp server, obs, lifecycle funcs on StreamShow

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamShow from './streams/StreamShow';
@@ -19,11 +19,13 @@ const App = () => {
         <div className="ui container">
             <Router history={history}>
             <Header />
+            <Switch>
                 <Route path="/" exact component={StreamList} />
                 <Route path="/streams/new" exact component={StreamCreate} />
                 <Route path="/streams/edit/:id" exact component={StreamEdit} />
                 <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                <Route path="/streams/show" exact component={StreamShow} />
+                <Route path="/streams/:id" exact component={StreamShow} />
+            </Switch>
             </Router>
         </div>
     );
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
-const StreamShow = () => {
-    return (
-        <div>StreamShow</div>
-    )
+class StreamShow extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.videoRef = React.createRef();
+    }
+
+    //when component first renders, attempt to buildPlayer() (but it won't happen 1st time)
+    componentDidMount() {
+        // console.log('this.props: ', this.props);
+        const { id } = this.props.match.params;
+        this.props.fetchStream(id);
+        this.buildPlayer();
+    }
+
+    //if component fetchStream successfully at some point and the component rerenders, buildPlayer again
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    //basically if at any point the component renders/rerenders, we attempt to build the player
+    //but if we've already built it (already have this.player) OR we dont have the stream, then we don't build it
+    buildPlayer() {
+        if(this.player || !this.props.stream) {
+            return;
+        };
+
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+    }
+
+    render() {
+        if(!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        const { title, description } = this.props.stream
+        //we can just have the name of the prop 'controls' as the attr instead of setting it to {true} ONLY when a prop's value is true
+        return (
+            <div>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls={true} />
+                <h1>{title}</h1>
+                <h1>{description}</h1>
+            </div>
+        )
+    }
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamShow);
